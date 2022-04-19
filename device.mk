a84200b5082b6d370eab655c7d9facd3c6a374ad
EduardoA3677 <ealvarado261500@gmail.com>
2022-04-19 16:07:06 -0500

    billie: Nuke updatable APEX
    * We dont support it in PE

--- device.mk
+++ device.mk
@@ -17,9 +17,6 @@
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
